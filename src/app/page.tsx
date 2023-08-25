030c9a84292fb9559c3fece65859c623989c843c
PALLdev <pblolara21@gmail.com>
2023-08-25 12:04:21 -0400

    added rating component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,77 @@
-'use client'
+"use client";
 
-import React, { useState } from "react"
-import data from "@/data.json"
+import React, { useState } from "react";
+import data from "@/data.json";
 
 const questions: Question[] = data.preguntas;
 
 export default function Home() {
-
-  const [answers, setAnswers] = useState<Answer[]>([])
+  const [answers, setAnswers] = useState<Answer[]>([]);
 
   // comienza vacio (length 0), por lo que muestra la primera pregunta
-  const currentQuestion = questions[answers.length]
+  const currentQuestion = questions[answers.length];
 
-  function handleRate(event: React.ChangeEvent<HTMLSelectElement>) {
-    const answerRate = Number(event.target.value) as Answer['valoracion']
-
+  function handleRate(rating: Answer["valoracion"]) {
     setAnswers((currentAnswers) =>
-      currentAnswers.concat({ ...currentQuestion, valoracion: answerRate })
-    )
+      currentAnswers.concat({ ...currentQuestion, valoracion: rating })
+    );
   }
 
   return (
-    <main>
-      {
-        answers.length > questions.length - 1 && !currentQuestion
-          ? <>
-            <div>Encuesta Completada</div>
-            <ul>
-              {
-                answers.map(answer => (
-                  <li key={answer.id}>{answer.texto} - <span>{answer.valoracion}</span></li>
-                ))
-              }
-            </ul>
-          </>
-          : <>
-            <h1>{currentQuestion.texto}</h1>
-            <select name="" id="" onChange={handleRate}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </>
-      }
-    </main>
-  )
+    <>
+      {answers.length > questions.length - 1 && !currentQuestion ? (
+        <div className="rounded-md border p-4">
+          <h2 className="text-3xl pb-4 text-center">Tus Respuestas</h2>
+          <ul>
+            {answers.map((answer) => (
+              <li
+                className="flex items-center justify-between text-2xl p-2"
+                key={answer.id}
+              >
+                <span className="">{answer.texto}</span>
+                <span>
+                  <Rating
+                    value={answer.valoracion}
+                    isReadOnly={true}
+                    onChange={() => {}}
+                  />
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="text-center text-2xl">
+          <h1 className="pb-2">{currentQuestion.texto}</h1>
+          <Rating value={1} onChange={handleRate} isReadOnly={false} />
+        </div>
+      )}
+    </>
+  );
 }
 
+// ☆ ★
+function Rating({ value, onChange, isReadOnly }: RatingProps) {
+  const [hoverValue, setHoverValue] = useState(value);
+  const pointerClass = !isReadOnly ? "cursor-pointer" : "cursor-default";
+
+  return "★"
+    .repeat(hoverValue)
+    .padEnd(5, "☆")
+    .split("")
+    .map((star, index) => (
+      <span
+        key={index}
+        onClick={() =>
+          !isReadOnly && onChange((index + 1) as Answer["valoracion"])
+        }
+        onMouseOver={() =>
+          !isReadOnly && setHoverValue((index + 1) as Answer["valoracion"])
+        }
+        onMouseLeave={() => !isReadOnly && setHoverValue(value)}
+        className={`text-amber-300 ${pointerClass}`}
+      >
+        {star}
+      </span>
+    ));
+}
--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -6,3 +6,9 @@ type Question = {
 type Answer = Question & {
     valoracion: 1 | 2 | 3 | 4 | 5
 }
+
+type RatingProps = {
+    value: Answer['valoracion'],
+    onChange: (value: Answer['valoracion']) => void
+    isReadOnly: boolean
+}
